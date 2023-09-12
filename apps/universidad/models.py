d32437f15cdfbe6c02a05d40d12e24dde210b99b
Jonathan Dominguez <jonathan24.dp@gmail.com>
2023-09-11 22:08:21 -0600

    Modify university model

--- apps/universidad/models.py
+++ apps/universidad/models.py
@@ -1,10 +1,13 @@
 # Importaciones Django
 from django.db import models
+# Importaciones locales
+from apps.equipo.models import Equipo
 
 
 class Universidad(models.Model):
   nombre = models.CharField(max_length=50)
   direccion = models.CharField(max_length=50)
+  idEquipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.nombre
