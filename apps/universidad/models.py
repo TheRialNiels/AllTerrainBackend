e445838b835f9fc1fcb682adbbf38ad3ecf03c9e
Jonathan Dominguez <jonathan24.dp@gmail.com>
2023-09-11 23:03:49 -0600

    Update model

--- apps/universidad/models.py
+++ apps/universidad/models.py
@@ -7,7 +7,7 @@ from apps.equipo.models import Equipo
 class Universidad(models.Model):
   nombre = models.CharField(max_length=50)
   direccion = models.CharField(max_length=50)
-  idEquipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+  idEquipo = models.ForeignKey(Equipo, on_delete=models.CASCADE, blank=True, null=True)
 
   def __str__(self):
     return self.nombre
