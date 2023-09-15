7d1eb0e997fa0a7fca478b21cfaf566ae4b29a11
Jonathan Dominguez <jonathan24.dp@gmail.com>
2023-09-15 00:53:34 -0600

    Add object manager

--- apps/encargado/models.py
+++ apps/encargado/models.py
@@ -10,6 +10,8 @@ class Encargado(models.Model):
   idUsuario = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
   idEquipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
 
+  objects = models.Manager()
+
   def __str__(self):
     return self.idUsuario.get_full_name()
 
--- apps/equipo/models.py
+++ apps/equipo/models.py
@@ -7,6 +7,8 @@ class Equipo(models.Model):
   puntaje = models.IntegerField(default=0, blank=True, null=True)
   status = models.BooleanField(default=True)
 
+  objects = models.Manager()
+
   def __str__(self):
     return self.nombreEquipo
 
