3760335ab1d38d5fdd4e5cf8930d5e949bb87750
Jonathan Dominguez <jonathan24.dp@gmail.com>
2023-09-14 23:42:34 -0600

    Update final urls

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,12 +1,15 @@
 # Django Imports
 from django.urls import path, include
+# Importaciones rest_framework
+from rest_framework.routers import DefaultRouter
 # Import Views
 from .views import UserAccountCreateView, UserAccountLoginView, UserAccountLogoutView, UserAccountChangePasswordView, RecoveryUserPassword, CurrentUser, GetUsers, UserAccountDeleteView, UserAccountUpdateView, UserAccountCreateEncargadoView
 
+router = DefaultRouter()
+router.register('create-user', UserAccountCreateEncargadoView, basename='create-user')
 
 urlpatterns = [
   path('create-user/', UserAccountCreateView.as_view(), name='create-user'),
-  path('create-encargado/', UserAccountCreateEncargadoView, name='create-encargado'),
   path('login/', UserAccountLoginView.as_view(), name='login'),
   path('logout/', UserAccountLogoutView.as_view(), name='logout'),
   path('change-password/', UserAccountChangePasswordView.as_view(),
@@ -17,4 +20,5 @@ urlpatterns = [
   path('get-users/', GetUsers.as_view(), name='get-users'),
   path('update-user/', UserAccountUpdateView.as_view(), name='update-user'),
   path('delete-user/<int:id>/', UserAccountDeleteView.as_view(), name='delete-user'),
+  path('', include(router.urls)),
 ]
