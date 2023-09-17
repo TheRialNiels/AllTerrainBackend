6ac7bdcfbbe0f6a2f0ba9a1423930a0e1c7665df
Jonathan Dominguez <jonathan24.dp@gmail.com>
2023-09-17 13:06:34 -0600

    Add equipo name in request

--- apps/universidad/serializer.py
+++ apps/universidad/serializer.py
@@ -5,6 +5,12 @@ from .models import Universidad
 
 
 class UniversidadSerializer(serializers.ModelSerializer):
+  nombre_equipo = serializers.SerializerMethodField()
   class Meta:
     model = Universidad
     fields = '__all__'
+
+  def get_nombre_equipo(self, obj):
+    if obj.idEquipo:
+      return obj.idEquipo.nombreEquipo
+    return None
